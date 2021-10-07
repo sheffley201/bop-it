9a6696e0001f1e6b4f21a1ca596c7b25b731d049
Spencer Heffley <spheffley@yahoo.com>
2021-10-07 11:09:47 -0400

    Removed double js from HTML. Basic logic is working

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
+const bopButton = $('.bop-it');
+const twistButton = $('.twist-it');
+const pullButton = $('.pull-it');
+let instructionText = $('.instruction');
 let started = false;
 let score = 0;
+let bop = false;
+let twist = false;
+let pull = false;
 
 const giveCommand = () => {
   if (score == 0) {
@@ -10,42 +17,72 @@ const giveCommand = () => {
     pullIt();
   } else {
     let command = randomCommand();
-    if (command == 0) {
+    console.log(command);
+    if (command == 1) {
       bopIt();
-    } else if (command == 1) {
-      twistIt();
     } else if (command == 2) {
+      twistIt();
+    } else if (command == 3) {
       pullIt();
     }
   }
 }
 
 const randomCommand = () => {
-  return Math.random() * (4 - 1) + 1;
+  return Math.floor(Math.random() * (4 - 1) + 1);
 }
 
 const bopIt = () => {
-
+  bop = true;
+  twist = false;
+  pull = false;
+  instructionText.text('Bop it!');
 }
 
 const twistIt = () => {
-
+  bop = false;
+  twist = true;
+  pull = false;
+  instructionText.text('Twist it!');
 }
 
 const pullIt = () => {
-
+  bop = false;
+  twist = false;
+  pull = true;
+  instructionText.text('Pull it!');
 }
 
-$(document).ready(function() {
-  const bop = $('.bop-it');
-  const twist = $('.twist-it');
-  const pull = $('.pull-it');
-  let instructionText = $('.instruction');
-  
-  bop.on("click", function() {
-    if (!started) {
-      started = true;
-      giveCommand();
-    }
-  });
-})
+const gameLose = () => {
+  instructionText.text('Game Over!');
+}
+
+bopButton.on("click", function() {
+  if (!started) {
+    started = true;
+    giveCommand();
+  } else if (bop) {
+    score++;
+    giveCommand();
+  } else {
+    gameLose();
+  }
+});
+
+twistButton.on("click", function() {
+  if (twist) {
+    score++;
+    giveCommand();
+  } else {
+    gameLose();
+  }
+});
+
+pullButton.on("click", function() {
+  if (pull) {
+    score++;
+    giveCommand();
+  } else {
+    gameLose();
+  }
+});
