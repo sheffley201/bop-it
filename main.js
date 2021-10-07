34f8e9996e8fdd9bbbb5bafb300ce2c5c1717677
Spencer Heffley <spheffley@yahoo.com>
2021-10-07 10:40:35 -0400

    Added js to HTML, redid JS using some jQuery

--- main.js
+++ main.js
@@ -1,19 +1,51 @@
-bopButton = document.querySelector("bop-it");
-twistButton = document.querySelector("twist-it");
-pullButton = document.querySelector("pull-it");
-instructionText = document.querySelector("instruction");
-gameArea = document.querySelector("game-area");
+let started = false;
 let score = 0;
 
-const startGame = () => {
-  bopButton.removeEventListener("click", startGame);
-  gameArea.addEventListener("click", bopIt);
-  bopIt("bop");
-};
-
-const bopIt = (instruction) => {
-  if (instruction == "bop") {
+const giveCommand = () => {
+  if (score == 0) {
+    bopIt();
+  } else if (score == 1) {
+    twistIt();
+  } else if (score == 2) {
+    pullIt();
+  } else {
+    let command = randomCommand();
+    if (command == 0) {
+      bopIt();
+    } else if (command == 1) {
+      twistIt();
+    } else if (command == 2) {
+      pullIt();
+    }
   }
-};
+}
 
-bopButton.addEventListener("click", startGame);
+const randomCommand = () => {
+  return Math.random() * (4 - 1) + 1;
+}
+
+const bopIt = () => {
+
+}
+
+const twistIt = () => {
+
+}
+
+const pullIt = () => {
+
+}
+
+$(document).ready(function() {
+  const bop = $('.bop-it');
+  const twist = $('.twist-it');
+  const pull = $('.pull-it');
+  let instructionText = $('.instruction');
+  
+  bop.on("click", function() {
+    if (!started) {
+      started = true;
+      giveCommand();
+    }
+  });
+})
